eec5256a4672dc437d943b69b45236bc1d45d10c
tyson ericksen <tysonericksen@tysons-MacBook-Air.local>
2019-12-18 14:32:33 -0700

    dragged outlets and actions for buttons

--- Tictactoe/ViewControllers/ViewController.swift
+++ Tictactoe/ViewControllers/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLeftButton: UIButton!
+    
+    @IBOutlet weak var topCenterButton: UIButton!
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var centerLeftButton: UIButton!
+    
+    @IBOutlet weak var centerCenterButton: UIButton!
+    @IBOutlet weak var centerRightButton: UIButton!
+    
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    
+    @IBOutlet weak var bottomCenterButton: UIButton!
+    
+    @IBOutlet weak var bottomRightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func topLeftButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func topCenterButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func topRightButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func centerLeftButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func centerCenterButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func centerRightButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func bottomLeftButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func bottomCenterButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func bottomRightButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
